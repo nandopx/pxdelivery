7317205695c1319bb1ef107029e0de84048ca254
Fernando Paixao <nandopox@gmail.com>
2021-01-05 22:45:35 -0300

    Project created

---
+++ backend-app/src/main/java/com/pxinfo/pxdelivery/PxdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.pxinfo.pxdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PxdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PxdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend-app/src/test/java/com/pxinfo/pxdelivery/PxdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pxinfo.pxdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PxdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
