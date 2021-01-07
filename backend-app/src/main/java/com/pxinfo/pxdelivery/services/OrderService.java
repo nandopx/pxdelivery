1d7ae5d184cb87779c1246044c71422e36984cea
Fernando Paixao <nandopox@gmail.com>
2021-01-06 21:11:32 -0300

    GET /Orders

---
+++ backend-app/src/main/java/com/pxinfo/pxdelivery/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.pxinfo.pxdelivery.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.pxinfo.pxdelivery.dto.OrderDTO;
+import com.pxinfo.pxdelivery.entities.Order;
+import com.pxinfo.pxdelivery.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll(){
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+}
